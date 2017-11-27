681b530780588e6148b7863ad4b1e118762dd763
Ye FengChun <yefengchun@github.com>
2017-11-27 19:48:06 +0800

    source format

--- chapter-02/sts/appdesign2/src/main/java/appdesign2/filter/DispatcherFilter.java
+++ chapter-02/sts/appdesign2/src/main/java/appdesign2/filter/DispatcherFilter.java
@@ -1,4 +1,5 @@
 package appdesign2.filter;
+
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -15,73 +16,66 @@ import appdesign2.form.ProductForm;
 import appdesign2.model.Product;
 import java.math.BigDecimal;
 
-@WebFilter(filterName = "DispatcherFilter",
-        urlPatterns = { "/*" })
+@WebFilter(filterName = "DispatcherFilter", urlPatterns = { "/*" })
 public class DispatcherFilter implements Filter {
 
-    @Override
-    public void init(FilterConfig filterConfig)
-            throws ServletException {
-    }
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
 
-    @Override
-    public void destroy() {
-    }
+	@Override
+	public void destroy() {
+	}
 
-    @Override
-    public void doFilter(ServletRequest request,
-            ServletResponse response, FilterChain filterChain)
-            throws IOException, ServletException {
-        HttpServletRequest req = (HttpServletRequest) request;
-        String uri = req.getRequestURI();
-        /*
-         * uri is in this form: /contextName/resourceName, for
-         * example /appdesign2/input-product. However, in the
-         * case of a default context, the context name is empty,
-         * and uri has this form /resourceName, e.g.:
-         * /input-product
-         */
-        // action processing
-        int lastIndex = uri.lastIndexOf("/");
-        String action = uri.substring(lastIndex + 1);
-        String dispatchUrl = null;
-        if ("input-product".equals(action)) {
-            // do nothing
-            dispatchUrl = "/jsp/ProductForm.jsp";
-        } else if ("save-product".equals(action)) {
-            // create form
-            ProductForm productForm = new ProductForm();
-            // populate action properties
-            productForm.setName(request.getParameter("name"));
-            productForm.setDescription(
-                    request.getParameter("description"));
-            productForm.setPrice(request.getParameter("price"));
-            
-            // create model
-            Product product = new Product();
-            product.setName(productForm.getName());
-            product.setDescription(product.getDescription());
-            try {
-                product.setPrice(new BigDecimal(productForm.getPrice()));
-            } catch (NumberFormatException e) {
-            }
-            // execute action method
-            SaveProductAction saveProductAction = 
-                    new SaveProductAction();
-            saveProductAction.save(product);
-            
-            // store model in a scope variable for the view
-            request.setAttribute("product", product);
-            dispatchUrl = "/jsp/ProductDetails.jsp";
-        }
-        // forward to a view
-        if (dispatchUrl != null) {
-            RequestDispatcher rd = request
-                    .getRequestDispatcher(dispatchUrl);
-            rd.forward(request, response);
-        } else {
-            // let static contents pass
-            filterChain.doFilter(request, response);
-        }
-    }
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		String uri = req.getRequestURI();
+		/*
+		 * uri is in this form: /contextName/resourceName, for example
+		 * /appdesign2/input-product. However, in the case of a default context, the
+		 * context name is empty, and uri has this form /resourceName, e.g.:
+		 * /input-product
+		 */
+		// action processing
+		int lastIndex = uri.lastIndexOf("/");
+		String action = uri.substring(lastIndex + 1);
+		String dispatchUrl = null;
+		if ("input-product".equals(action)) {
+			// do nothing
+			dispatchUrl = "/jsp/ProductForm.jsp";
+		} else if ("save-product".equals(action)) {
+			// create form
+			ProductForm productForm = new ProductForm();
+			// populate action properties
+			productForm.setName(request.getParameter("name"));
+			productForm.setDescription(request.getParameter("description"));
+			productForm.setPrice(request.getParameter("price"));
+
+			// create model
+			Product product = new Product();
+			product.setName(productForm.getName());
+			product.setDescription(product.getDescription());
+			try {
+				product.setPrice(new BigDecimal(productForm.getPrice()));
+			} catch (NumberFormatException e) {
+			}
+			// execute action method
+			SaveProductAction saveProductAction = new SaveProductAction();
+			saveProductAction.save(product);
+
+			// store model in a scope variable for the view
+			request.setAttribute("product", product);
+			dispatchUrl = "/jsp/ProductDetails.jsp";
+		}
+		// forward to a view
+		if (dispatchUrl != null) {
+			RequestDispatcher rd = request.getRequestDispatcher(dispatchUrl);
+			rd.forward(request, response);
+		} else {
+			// let static contents pass
+			filterChain.doFilter(request, response);
+		}
+	}
 }
--- chapter-02/sts/appdesign3/src/main/java/appdesign3/controller/ControllerServlet.java
+++ chapter-02/sts/appdesign3/src/main/java/appdesign3/controller/ControllerServlet.java
@@ -1,4 +1,5 @@
 package appdesign3.controller;
+
 import java.io.IOException;
 import java.util.List;
 import javax.servlet.RequestDispatcher;
@@ -13,88 +14,74 @@ import appdesign3.model.Product;
 import appdesign3.validator.ProductValidator;
 import java.math.BigDecimal;
 
-@WebServlet(name = "ControllerServlet", urlPatterns = { 
-        "/input-product", "/save-product" })
+@WebServlet(name = "ControllerServlet", urlPatterns = { "/input-product", "/save-product" })
 public class ControllerServlet extends HttpServlet {
-    
-    private static final long serialVersionUID = 98279L;
 
-    @Override
-    public void doGet(HttpServletRequest request, 
-            HttpServletResponse response)
-            throws IOException, ServletException {
-        process(request, response);
-    }
+	private static final long serialVersionUID = 98279L;
 
-    @Override
-    public void doPost(HttpServletRequest request, 
-            HttpServletResponse response)
-            throws IOException, ServletException {
-        process(request, response);
-    }
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		process(request, response);
+	}
 
-    private void process(HttpServletRequest request,
-            HttpServletResponse response) 
-            throws IOException, ServletException {
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		process(request, response);
+	}
 
-        String uri = request.getRequestURI();
-        /*
-         * uri is in this form: /contextName/resourceName, 
-         * for example: /appdesign1/input-product. 
-         * However, in the case of a default context, the 
-         * context name is empty, and uri has this form
-         * /resourceName, e.g.: /input-product
-         */
-        int lastIndex = uri.lastIndexOf("/");
-        String action = uri.substring(lastIndex + 1); 
-        String dispatchUrl = null;
+	private void process(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
 
-        if ("input-product".equals(action)) {
-            // no action class, there is nothing to be done
-            dispatchUrl = "/jsp/ProductForm.jsp";
-        } else if ("save-product".equals(action)) {
-            // instantiate action class
-            ProductForm productForm = new ProductForm();
-            // populate action properties
-            productForm.setName(
-                    request.getParameter("name"));
-            productForm.setDescription(
-                    request.getParameter("description"));
-            productForm.setPrice(request.getParameter("price"));
-            
-            // validate ProductForm
-            ProductValidator productValidator = new
-                    ProductValidator();
-            List<String> errors = 
-                    productValidator.validate(productForm);
-            if (errors.isEmpty()) {
-                // create Product from ProductForm
-                Product product = new Product();
-                product.setName(productForm.getName());
-                product.setDescription(
-                        productForm.getDescription());
-                product.setPrice(new BigDecimal(productForm.getPrice()));
-                
-                // no validation error, execute action method
-                SaveProductAction saveProductAction = new 
-                        SaveProductAction();
-                saveProductAction.save(product);
-                
-                // store action in a scope variable for the view
-                request.setAttribute("product", product);
-                dispatchUrl = "/jsp/ProductDetails.jsp";
-            } else {
-                request.setAttribute("errors", errors);
-                request.setAttribute("form", productForm);
-                dispatchUrl = "/jsp/ProductForm.jsp";
-            }
-        }
+		String uri = request.getRequestURI();
+		/*
+		 * uri is in this form: /contextName/resourceName, for example:
+		 * /appdesign1/input-product. However, in the case of a default context, the
+		 * context name is empty, and uri has this form /resourceName, e.g.:
+		 * /input-product
+		 */
+		int lastIndex = uri.lastIndexOf("/");
+		String action = uri.substring(lastIndex + 1);
+		String dispatchUrl = null;
 
-        // forward to a view
-        if (dispatchUrl != null) {
-            RequestDispatcher rd = 
-                    request.getRequestDispatcher(dispatchUrl);
-            rd.forward(request, response);
-        }
-    }
+		if ("input-product".equals(action)) {
+			// no action class, there is nothing to be done
+			dispatchUrl = "/jsp/ProductForm.jsp";
+		} else if ("save-product".equals(action)) {
+			// instantiate action class
+			ProductForm productForm = new ProductForm();
+			// populate action properties
+			productForm.setName(request.getParameter("name"));
+			productForm.setDescription(request.getParameter("description"));
+			productForm.setPrice(request.getParameter("price"));
+
+			// validate ProductForm
+			ProductValidator productValidator = new ProductValidator();
+			List<String> errors = productValidator.validate(productForm);
+			if (errors.isEmpty()) {
+				// create Product from ProductForm
+				Product product = new Product();
+				product.setName(productForm.getName());
+				product.setDescription(productForm.getDescription());
+				product.setPrice(new BigDecimal(productForm.getPrice()));
+
+				// no validation error, execute action method
+				SaveProductAction saveProductAction = new SaveProductAction();
+				saveProductAction.save(product);
+
+				// store action in a scope variable for the view
+				request.setAttribute("product", product);
+				dispatchUrl = "/jsp/ProductDetails.jsp";
+			} else {
+				request.setAttribute("errors", errors);
+				request.setAttribute("form", productForm);
+				dispatchUrl = "/jsp/ProductForm.jsp";
+			}
+		}
+
+		// forward to a view
+		if (dispatchUrl != null) {
+			RequestDispatcher rd = request.getRequestDispatcher(dispatchUrl);
+			rd.forward(request, response);
+		}
+	}
 }
