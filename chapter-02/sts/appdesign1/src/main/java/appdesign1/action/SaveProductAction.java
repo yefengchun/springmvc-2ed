cf431ce9aae940d3d2afac8208e94567396b9f5c
Ye FengChun <yefengchun@github.com>
2017-11-27 18:04:10 +0800

    print a line to console on SaveProductAction

--- chapter-02/sts/appdesign1/src/main/java/appdesign1/action/SaveProductAction.java
+++ chapter-02/sts/appdesign1/src/main/java/appdesign1/action/SaveProductAction.java
@@ -2,8 +2,10 @@ package appdesign1.action;
 
 import appdesign1.model.Product;
 
+
 public class SaveProductAction {
 	public void save(Product product) {
 		// insert Product to the database
+		System.out.println("saved " + product.getName() + "\t" + new java.util.Date());
 	}
 }
